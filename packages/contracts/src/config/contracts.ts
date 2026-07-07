8c66df6b9c7219faf86ea1f5e1b98399d61a2a53
Mrfuture625 <futuregrid625@gmail.com>
2026-07-07 08:19:02 +0530

    Update contract ABI and addresses

--- packages/contracts/src/config/contracts.ts
+++ packages/contracts/src/config/contracts.ts
@@ -1,8 +1,8 @@
 export const KRYPTPAY_CONTRACTS = {
   kpayToken: "0xB84357f221EcD21f41fd9A47B6ea86e7b33380A2",
   rewardClaim: "0x08bf745dcf0E04c4eb801E1744248db5263445af",
-  cardMarketplace: "0xF534aA0Da9bC86eAe2B9a5f1dd68288fb5f656CC",
-  vault: "0xc7a986A5d83967736e5D7EC0882030947924916c",
+  cardMarketplace: "0x1657a8c0965f5Cd5323d69d098843d58Ed92d8B4",
+  vault: "0xa7B0e549B53a7317154519c56c37162CBA0d6011",
 } as const;
 
 export const CARD_TYPE = {
